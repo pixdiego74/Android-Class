f75beab96163112d343e4d03192dbdbda9fdc572
pixdiego74 <st4059@utr.edu.mx>
2026-03-02 11:11:11 -0600

    Avance index

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,29 @@
 import React, { useState } from "react";
-import { Text, View } from "react-native";
+import { Alert, StyleSheet, Text, View } from "react-native";
 
 const GeneradorSaludos = () => {
   //State definition
   const [name, setName] = useState('');
   const [age, setAge] = useState('');
+
+  //Function to trigger the action btn
+  const handleBtnHello = () => {
+    //Empty space validation
+    if (name.trim() === '' || age.trim() === ''){
+      Alert.alert('Error', 'Please, all fields are required');
+      return;
+    }
+    //Show alert with hello message
+    Alert.alert('Welcome', `Hello, ${name} you are ${age} years old.`);
+
+    //Clean
+    setName('');
+    setAge('');
+  }
+  //Structure
+  return (
+    <View style={styles.container}></View>
+  )
 }
 
 export default function Index() {
@@ -20,3 +39,29 @@ export default function Index() {
     </View>
   );
 }
+
+  const styles =  StyleSheet.create({
+    container: {
+      flex: 1,
+      padding: 20,
+      justifyContent: 'center',
+      backgroundColor: '#F5F5F5'
+    },
+    title: {
+      fontSize: 24,
+      fontWeight: 'bold',
+      marginBottom: 20,
+      textAlign: 'center',
+      color: '#333'
+    },
+    input: {
+      height: 50,
+      borderColor: '#cccccc',
+      borderWidth: 1,
+      borderRadius: 8,
+      paddingHorizontal: 15,
+      marginBottom: 15,
+      backgroundColor: '#ffffff',
+      fontSize: 16
+    }
+  });
